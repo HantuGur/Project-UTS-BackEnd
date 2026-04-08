e2eae1453ac1439e19ea92b58b9d4661d88a0772
aaron-valerio <aaronvaleriolee@gmail.com>
2026-04-08 12:02:15 +0700

    Update menuController.js

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -33,6 +33,39 @@ const createMenu = async (req, res) => {
   }
 };
 
+const getMenuById = async (req, res) => {
+  try {
+    const menu = await Menu.findById(req.params.id).populate('category', 'name');
+    if (!menu) {
+      return res.status(404).json({ success: false, message: 'Menu tidak ditemukan' });
+    }
+    res.status(200).json({ success: true, data: menu });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// [ANGGOTA 2] PUT update menu
+const updateMenu = async (req, res) => {
+  try {
+    const menu = await Menu.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    }).populate('category', 'name');
+
+    if (!menu) {
+      return res.status(404).json({ success: false, message: 'Menu tidak ditemukan' });
+    }
+    res.status(200).json({
+      success: true,
+      message: 'Menu berhasil diupdate',
+      data: menu,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
 const deleteMenu = async (req, res) => {
   try {
     const menu = await Menu.findByIdAndDelete(req.params.id);
